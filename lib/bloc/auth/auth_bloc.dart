7ccd3521d9f44b9b763f361b6147e90b3ff32239
Ilkhom I. Melibaev <107622192+fluttberry@users.noreply.github.com>
2025-05-19 16:38:33 +0600

    auth bloc set

--- lib/bloc/auth/auth_bloc.dart
+++ lib/bloc/auth/auth_bloc.dart
@@ -1,12 +1,16 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ibonus_app/bloc/auth/auth_event.dart';
 import 'package:ibonus_app/bloc/auth/auth_state.dart';
+import 'package:ibonus_app/repository/auth_repository.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc (): super (AuthState()){
-on <AuthEventGetCity>(_getCity);
+  AuthBloc() : super(AuthState()) {
+    on<AuthEventGetCity>(_getCity);
   }
-  _getCity(AuthEventGetCity event, emit){
-    
+
+  AuthRepository authRepository = AuthRepository();
+  _getCity(AuthEventGetCity event, emit) async {
+    var cities = await authRepository.getCities();
+    emit(AuthState(cityModel: cities));
   }
 }
--- lib/bloc/auth/auth_state.dart
+++ lib/bloc/auth/auth_state.dart
@@ -2,4 +2,5 @@ import 'package:ibonus_app/model/city_model.dart';
 
 class AuthState {
   CityModel? cityModel;
+  AuthState ({this.cityModel});
 }
