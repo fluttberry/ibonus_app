03b8b250b291491386f82578e3d86d0b90821e72
Ilkhom I. Melibaev <107622192+fluttberry@users.noreply.github.com>
2025-05-30 17:51:55 +0600

    store repository done

---
+++ lib/repository/store_repository.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:ibonus_app/data/shared_pref.dart';
+import 'package:ibonus_app/model/store_model.dart';
+
+class StoreRepository {
+  Future<StoreModel?> get() async {
+    var response = await http.get(
+      Uri.parse('https://back.ibonus.app/api/v1/stores/'),
+      headers: {'Authorization': 'token ${SharedPref.getToken()}'},
+    );
+    if (response.statusCode == 200) {
+      return StoreModel.fromJson(jsonDecode(response.body));
+    }
+    return null; 
+  }
+}
